becebc400593619d382fe255037649e1c1a336f3
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-08-05 18:41:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-31.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
