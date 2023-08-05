55ee19022b7fc6b9f5050dd7edbd2e863649e8af
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-08-05 18:37:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  const realData = [];
+  console.log(socket.id);
+  socket.on("join_the_room", (data) => {
+    socket.join("id:", data);
+    console.log(`User:${socket.id} connected with room id:${data}`);
+  });
+  socket.on("send_the_message", (data) => {
+    console.log("room id is: ", data.roomId);
+    realData.push(data);
+    socket.broadcast.emit("get_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected user:", socket.id);
+  });
+}); //events
+
+server.listen(3001, () => {
+  console.log("Server is listening on port 3001");
+});
