19e055f27d856bf0107f4dab470263099df5abf3
prabhjot-arora31 <83449967+prabhjot-arora31@users.noreply.github.com>
2023-08-05 18:52:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+app.get("/",(req,res)=>{
+  res.send("responded.....");
+});
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
